839c29e96c33a35683c418566593a7a877b08715
Marcin Rajwa <marcin.rajwa@linux.intel.com>
2020-08-26 15:15:42 +0200

    component: add "codec adapter" component
    This introduces new SoF component called "codec adapter".
    It provides means to process samples through external codec
    libraries.
    
    Signed-off-by: Marcin Rajwa <marcin.rajwa@linux.intel.com>

---
+++ src/audio/codec_adapter/codec_adapter.c
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: BSD-3-Clause
+//
+// Copyright(c) 2020 Intel Corporation. All rights reserved.
+//
+// Author: Marcin Rajwa <marcin.rajwa@linux.intel.com>
+
+/*
+ * A codec adapter component.
+ */
+
+/**
+ * \file audio/codec_adapter.c
+ * \brief Processing compoent aimed to work with external codec libraries
+ * \author Marcin Rajwa <marcin.rajwa@linux.intel.com>
+ */
+
+#include <sof/audio/buffer.h>
+#include <sof/audio/component.h>
+#include <sof/audio/pipeline.h>
+#include <sof/common.h>
+
+/* d8218443-5ff3-4a4c-b388-6cfe07b956aa */
+DECLARE_SOF_RT_UUID("codec_adapter", ca_uuid, 0xd8218443, 0x5ff3, 0x4a4c,
+		    0xb3, 0x88, 0x6c, 0xfe, 0x07, 0xb9, 0x56, 0xaa);
+
+DECLARE_TR_CTX(ca_tr, SOF_UUID(ca_uuid), LOG_LEVEL_INFO);
+
