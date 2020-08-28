e09e75ab41d1ae59188eefcfa566e1782d4e9a1c
Marcin Rajwa <marcin.rajwa@linux.intel.com>
2020-08-28 12:11:32 +0200

    codec_generic: add validation of setup config
    This add dummy validation function for hifi codec setup config.
    
    Signed-off-by: Marcin Rajwa <marcin.rajwa@linux.intel.com>

--- src/audio/codec_adapter/codec/generic.c
+++ src/audio/codec_adapter/codec/generic.c
@@ -13,6 +13,12 @@
 
 static struct codec_data codec_data;
 
+static int validate_config(struct hifi_codec_config *cfg)
+{
+	//TODO:
+	return 0;
+}
+
 int codec_load_config(struct comp_dev *dev, void *cfg, size_t size,
 		       enum codec_cfg_type type)
 {
@@ -40,6 +46,13 @@ int codec_load_config(struct comp_dev *dev, void *cfg, size_t size,
 
 	ret = memcpy_s(dst->data, size, cfg, size);
 	assert(!ret);
+
+	ret = validate_config(dst->data);
+	if (ret) {
+		comp_err(dev, "codec_load_config() error: validation of config failed!");
+		ret = -EINVAL;
+		goto err;
+	}
 
 	/* Config loaded, mark it as valid */
 	dst->size = size;
