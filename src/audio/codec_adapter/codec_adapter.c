900fc1946dca18ebf6ac60ddd87debb42c9340f9
Marcin Rajwa <marcin.rajwa@linux.intel.com>
2020-09-27 18:47:04 +0200

    codec_adapter: add .create() method
    This adds .create() method to codec adapter which allows new SoF
    component creation. The method introduced here is part one
    performing only basic tasks like memory allocation.
    
    Signed-off-by: Marcin Rajwa <marcin.rajwa@linux.intel.com>

--- src/audio/codec_adapter/codec_adapter.c
+++ src/audio/codec_adapter/codec_adapter.c
@@ -16,8 +16,11 @@
 
 #include <sof/audio/buffer.h>
 #include <sof/audio/component.h>
+#include <sof/audio/codec_adapter/codec_adapter.h>
 #include <sof/audio/pipeline.h>
 #include <sof/common.h>
+
+static const struct comp_driver comp_codec_adapter;
 
 /* d8218443-5ff3-4a4c-b388-6cfe07b956aa */
 DECLARE_SOF_RT_UUID("codec_adapter", ca_uuid, 0xd8218443, 0x5ff3, 0x4a4c,
@@ -25,3 +28,62 @@ DECLARE_SOF_RT_UUID("codec_adapter", ca_uuid, 0xd8218443, 0x5ff3, 0x4a4c,
 
 DECLARE_TR_CTX(ca_tr, SOF_UUID(ca_uuid), LOG_LEVEL_INFO);
 
+
+/**
+ * \brief Create a codec adapter component.
+ * \param[in] drv - component driver pointer.
+ * \param[in] comp - component ipc descriptor pointer.
+ *
+ * \return: a pointer to newly created codec adapter component.
+ */
+static struct comp_dev *codec_adapter_new(const struct comp_driver *drv,
+					  struct sof_ipc_comp *comp)
+{
+	int ret;
+	struct comp_dev *dev;
+	struct comp_data *cd;
+
+	comp_cl_info(&comp_codec_adapter, "codec_adapter_new() start");
+
+	if (!drv || !comp) {
+		comp_cl_err(&comp_codec_adapter, "codec_adapter_new(), wrong input params! drv = %x comp = %x",
+			    (uint32_t)drv, (uint32_t)comp);
+		return NULL;
+	}
+
+	dev = comp_alloc(drv, COMP_SIZE(struct sof_ipc_comp_process));
+	if (!dev) {
+		comp_cl_err(&comp_codec_adapter, "codec_adapter_new(), failed to allocate memory for comp_dev");
+		return NULL;
+	}
+
+	dev->drv = drv;
+
+	ret = memcpy_s(&dev->comp, sizeof(struct sof_ipc_comp_process),
+		       comp, sizeof(struct sof_ipc_comp_process));
+	assert(!ret);
+
+	cd = rzalloc(SOF_MEM_ZONE_RUNTIME, 0, SOF_MEM_CAPS_RAM, sizeof(*cd));
+	if (!cd) {
+		comp_cl_err(&comp_codec_adapter, "codec_adapter_new(), failed to allocate memory for comp_data");
+		rfree(dev);
+		return NULL;
+	}
+
+	comp_set_drvdata(dev, cd);
+
+	dev->state = COMP_STATE_READY;
+	cd->state = PP_STATE_CREATED;
+
+	comp_cl_info(&comp_codec_adapter, "codec_adapter_new() done");
+	return dev;
+}
+
+static const struct comp_driver comp_codec_adapter = {
+	.type = SOF_COMP_NONE,
+	.uid = SOF_RT_UUID(ca_uuid),
+	.tctx = &ca_tr,
+	.ops = {
+		.create = codec_adapter_new,
+	},
+};
---
+++ src/include/sof/audio/codec_adapter/codec_adapter.h
@@ -0,0 +1,13 @@
+/* SPDX-License-Identifier: BSD-3-Clause
+ *
+ * Copyright(c) 2020 Intel Corporation. All rights reserved.
+ *
+ * Author: Marcin Rajwa <marcin.rajwa@linux.intel.com>
+ */
+
+#ifndef __SOF_AUDIO_CODEC_ADAPTER__
+#define __SOF_AUDIO_CODEC_ADAPTER__
+
+#include <sof/audio/codec_adapter/codec/generic.h>
+
+#endif /* __SOF_AUDIO_CODEC_ADAPTER__ */
